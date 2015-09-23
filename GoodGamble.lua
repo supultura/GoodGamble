b23c698ada3705bd8d20714e8b8f9275e91f2dde
supultura <supulturastreaming@gmail.com>
2015-09-23 20:33:24 +1000

    Plan for lua side of addon

--- GoodGamble.lua
+++ GoodGamble.lua
@@ -8,18 +8,47 @@ end
 local function PrintMessage(msg)
 	DEFAULT_CHAT_FRAME:AddMessage(msg);
 end
-
-
-
-
 --TODO--
+--#1 PRIORITY--
+--XML AND COMMENTS--
+
+--Init--
+	--Check if Player has loaded(Blizzard API: PlayerLoadedWorld(?))
+	--Chat Channel default
+--Chat--
+	--Cycle through Instance(i) Guild (g) Whisper (w/r)
+	--sets default channel for entire program
+--Chat Message Handler--
+	--Same as PrintMessage but with Chat Method passed in
+	--overloading in Lua?
 --ResetGame--
+	--break current function
+	--Stop listening to 1s, Clear arrays/tables
 --EntryListener--
+	--check 1s
 	--list of entered players
 	--remove player from list
---RollGame--
-	--ask for roll
-	--
+--CheckRolls(EnteredPlayers)--
+	--ask for rolls
+	--listen for rolls, add rolls + players to table, remove players from entered players
+	--handle draw
+--CompareRolls(TableOfRolls)--
+	--sort table
+	--[0] - [SizeOfTable] = amount owed, Player1 owes Player2 x gold
+	--Check if player is on stats if not add if is -/+
+--GameModes--
+	--Standard
+		--2 or more people roll lowest roll pays difference to highest roll
+	--Roll Tournament
+		--2^x amount of people in roll tournament (possible seeding based off stats)(seeding if not a power of 2 people selected) 1v1 against each other until their is a champion
+		--Tournament winner stats
+	--Other Games TBA
+		--possible roulette reference?
+--Stats--
+	--table |Player|Gold|
+--PrintStats--
+	--Sort table
+	--print Name has won/lost gold depending on negative
 --ResetStats--
 local function ResetStats()
 	GoodGamble["Stats"] = {};
